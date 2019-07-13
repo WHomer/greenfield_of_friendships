c6a03b7f1d29e69f6f05266396b4d2a31867de5b
William Homer <billyhomeriii@gmail.com>
2019-07-13 01:56:38 -0600

    add applciationrecord and confirm AR is working

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require './app/models/friendship'
 
 class App < Sinatra::Base
   get "/" do
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,3 +1,5 @@
+require_relative 'application_record'
+
 class Friendship < ApplicationRecord
 
 end
